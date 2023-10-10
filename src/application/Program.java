e0663191b7f3922cfe34aaae04ecaae5bed4a579
Rafael Bezerra <jbezerra@faeterj-petropolis.edu.br>
2023-10-09 21:30:20 -0300

    FindById implematation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,20 +1,18 @@
 package application;
 
-import java.util.Date;
-
-import model.entities.Department;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		Department obj = new Department(1, "Livros");
-		Seller obj2 = new Seller(1,"Bruno Lange","Bruno@gmail.com",new Date(),1000.0,obj);;
+		Seller seller = sellerDao.findById(3);
 		
-		System.out.println(obj);
-		System.out.println(obj2);
+		System.out.println(seller);
 	}
 
 }
